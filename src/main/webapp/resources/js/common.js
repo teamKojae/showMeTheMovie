c3844d7abfd5f7e06d46aba0218d6277ebbc92a2
Kojae <sle1392@naver.com>
2020-03-17 17:29:49 +0900

    서막의 시작..

---
+++ src/main/webapp/resources/js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    	'<object type="text/html" data="D:\STSworkspace\showMeTheMovie\src\main\webapp\resources\common\header.html"></object>';
+})
+
+
+$(document).ready(function(){
+	$('#header').load("D:\STSworkspace\showMeTheMovie\src\main\webapp\resources\common\header.html");
+	
+})
+
+
+
+
