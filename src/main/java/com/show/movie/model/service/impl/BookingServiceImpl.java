17ee9de784b3503ff830213f66ea1655d707c3f2
kojae <sle1392@naver.com>
2020-04-09 01:44:59 +0900

    주석제거

--- src/main/java/com/show/movie/model/service/impl/BookingServiceImpl.java
+++ src/main/java/com/show/movie/model/service/impl/BookingServiceImpl.java
@@ -45,6 +45,7 @@ public class BookingServiceImpl implements BookingService {
 
 	@Override
 	public void cancelDate(int bookingCode) {
+		cancellationDAO.updateMovieSeat(bookingCode);
 		cancellationDAO.cancelDate(bookingCode);
 		
 	}
